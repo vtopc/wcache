4847e674ab7d1f99c4c039a83c565c0c56e867b0
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2019-12-28 12:57:39 +0200

    added get()

--- cache.go
+++ cache.go
@@ -2,7 +2,6 @@ package wcache
 
 import (
 	"context"
-	"errors"
 	"sync"
 	"time"
 )
@@ -52,22 +51,16 @@ func (c *Cache) Set(key, value interface{}) error {
 
 // SetWithTTL sets the value for a key.
 func (c *Cache) SetWithTTL(key, value interface{}, ttl time.Duration) error {
-	v, ok := c.m.Load(key)
-	if ok {
+	i, found := c.get(key)
+	if found {
 		// update
-		var ok bool
-		i, ok := v.(item)
-		if !ok {
-			return errors.New("internal error: failed to assert item")
-		}
-
 		i.setter <- value
 
 		return nil
 	}
 
 	// create new one
-	i := newItem()
+	i = newItem()
 	go c.runVault(key, value, i, ttl)
 	c.m.Store(key, i)
 
@@ -76,23 +69,34 @@ func (c *Cache) SetWithTTL(key, value interface{}, ttl time.Duration) error {
 
 // Get returns the value stored in the map for a key, or nil if no
 // value is present.
-// The ok result indicates whether value was found in the map.
-func (c *Cache) Get(key interface{}) (value interface{}, ok bool) {
-	v, ok := c.m.Load(key)
-	if !ok {
+// The found result indicates whether value was found in the map.
+func (c *Cache) Get(key interface{}) (value interface{}, found bool) {
+	i, found := c.get(key)
+	if !found {
 		return nil, false
 	}
 
-	i := v.(item)
-	value = <-i.getter
-
-	return value, true
+	return <-i.getter, true
 }
 
 // TODO: add instead of context?
 // func (c *Cache) Sync() {
 // 	c.m.Range()
 // }
+
+func (c *Cache) get(key interface{}) (item, bool) {
+	v, found := c.m.Load(key)
+	if !found {
+		return item{}, false
+	}
+
+	i, ok := v.(item)
+	if !ok {
+		panic("internal error: failed to assert item")
+	}
+
+	return i, true
+}
 
 // runVault - creates storage for value
 func (c *Cache) runVault(key, value interface{}, i item, ttl time.Duration) {
