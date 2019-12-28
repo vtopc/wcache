2cf8763e8908a6edfe82428854a5506b64da8b8b
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2019-12-28 16:52:32 +0200

    better comments

--- example_test.go
+++ example_test.go
@@ -9,9 +9,9 @@ import (
 
 func Example() {
 	c := wcache.New(context.Background(), 100*time.Millisecond, wcache.PrintlnOnExpire)
-	// with custom TTL:
+	// put value with custom TTL:
 	c.SetWithTTL(2, "to expire second", 200*time.Millisecond)
-	// with default TTL:
+	// put value with default TTL:
 	c.Set(1, "to expire first")
 
 	time.Sleep(300 * time.Millisecond)
