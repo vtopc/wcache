02efda17a57e2a46bf0eba5daef459932c73a7b1
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2019-12-30 14:01:29 +0200

    added ChanExpire

--- expire_funcs.go
+++ expire_funcs.go
@@ -7,12 +7,25 @@ import (
 // ExpireFn a callback that will be called when record is expired
 type ExpireFn func(key, value interface{})
 
+type KeyValue struct {
+	Key   interface{}
+	Value interface{}
+}
+
 // NoopExpire does nothing
 func NoopExpire(key, value interface{}) {}
 
-// PrintOnExpire a dummy ExpireFn that will print key value when record is expired
+// PrintOnExpire a dummy ExpireFn that will print key, value when record is expired
 func PrintlnOnExpire(key, value interface{}) {
 	fmt.Printf("%d: %s\n", key, value)
 }
 
-// TODO: add ChanExpire
+// ChanExpire returns a ExpireFn that will send key, value to the channel `ch` when record is expired
+func ChanExpire(ch chan<- KeyValue) ExpireFn {
+	return func(key, value interface{}) {
+		ch <- KeyValue{
+			Key:   key,
+			Value: value,
+		}
+	}
+}
