95a4afce3b3bcbffd268e6d5c59adb0071fea734
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2019-12-30 21:14:27 +0200

    Switch to regular map and mutex (#1)
    * switched to map[string]item with mutex
    
    * added linter

--- expire_funcs.go
+++ expire_funcs.go
@@ -5,24 +5,24 @@ import (
 )
 
 // ExpireFn a callback that will be called when record is expired
-type ExpireFn func(key, value interface{})
+type ExpireFn func(key string, value interface{})
 
 type KeyValue struct {
-	Key   interface{}
+	Key   string
 	Value interface{}
 }
 
 // NoopExpire does nothing
-func NoopExpire(key, value interface{}) {}
+func NoopExpire(key string, value interface{}) {}
 
 // PrintOnExpire a dummy ExpireFn that will print key, value when record is expired
-func PrintlnOnExpire(key, value interface{}) {
-	fmt.Printf("%v: %v\n", key, value)
+func PrintlnOnExpire(key string, value interface{}) {
+	fmt.Printf("%s: %v\n", key, value)
 }
 
 // ChanExpire returns a ExpireFn that will send key, value to the channel `ch` when record is expired
 func ChanExpire(ch chan<- KeyValue) ExpireFn {
-	return func(key, value interface{}) {
+	return func(key string, value interface{}) {
 		ch <- KeyValue{
 			Key:   key,
 			Value: value,
