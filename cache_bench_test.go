62f55aa1ab390fd82075ad523c9e011b1e67f2be
vtopc <32271530+vtopc@users.noreply.github.com>
2020-01-01 17:55:01 +0200

    fixed bench

--- cache_bench_test.go
+++ cache_bench_test.go
@@ -68,8 +68,9 @@ func BenchmarkSetDeleteRandom(b *testing.B) {
 	c := New(context.Background(), time.Hour, NoopExpire)
 
 	for n := 0; n < b.N; n++ {
+		key := uuid.New().String()
 		c.Set(uuid.New().String(), value)
-		// TODO: call Delete!
+		c.Delete(key)
 	}
 }
 
